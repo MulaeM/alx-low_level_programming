03cc8f59f47e67bd06aa56a9111172dbe38bb2af
MualeM <mulualem00m@gmail.com>
2022-07-07 13:50:56 -0700

    4. I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,9 @@
 
 /**
  * print_most_numbers - prints the numbers since 0 up to 9
+ *
  * Description: Prints the numbers excluding 2 and 4
+ *
  * Return: The numbers since 0 up to 9
  */
 void print_most_numbers(void)
@@ -17,7 +19,7 @@ void print_most_numbers(void)
 		}
 		else
 		{
-			_putchar(x = '0');
+			_putchar(x + '0');
 		}
 	}
 	_putchar('\n');
