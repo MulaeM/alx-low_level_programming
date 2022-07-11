3e3706f0576be9a78374a16c2e32466e3eddb0f3
MualeM <mulualem00m@gmail.com>
2022-07-11 14:29:13 -0700

    task 9. strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcpy - copies the string pointed to by src
+ * @dest: this is destiny
+ * @src: this is the copy
+ *
+ * Return: this return copy
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *start = dest;
+
+	while (src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest != '\0';
+	return (start);
+}
