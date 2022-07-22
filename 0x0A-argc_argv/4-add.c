2c78ddc7dc7405e1b7f7f24b9d2e007029a3909b
MualeM <mulualem00m@gmail.com>
2022-07-22 13:10:11 -0700

    Task 4. To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -20,3 +20,34 @@ int isInteger(const char *s)
 	}
 	return (0);
 }
+
+/**
+ * main - adds positive numbers
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+
+
+
+int main(int argc, char const *argv[])
+
+{
+
+		int sum = 0;
+
+
+
+			while (--argc)
+			{
+				if (isInteger(argv[argc]))
+			{												printf("Error\n");
+				return (1);
+			}
+				sum += atoi(argv[argc]);
+			}
+
+				printf("%i\n", sum);
+
+					return (0);
+}
