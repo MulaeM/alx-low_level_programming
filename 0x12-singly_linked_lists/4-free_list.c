b496e1c07d39dd635ac8331569aeab031b371ef3
MualeM <mulualem00m@gmail.com>
2022-08-12 13:21:15 -0700

    Task 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -13,7 +13,9 @@ void free_list(list_t *head)
 
 	while ((current = head) != NULL)
 	{
-		head = head->next;								free(current->str);								free(current);
+		head = head->next;
+		free(current->str);
+		free(current);
 	}
 
 }
